19e9340e3e64580f2d807531eaac46fd50398378
Chipili Kafwilo <forward.ingenuity07@gmail.com>
2017-12-04 19:30:07 +0200

    first commit

---
+++ www/js/ajaxSend.js
@@ -0,0 +1,39 @@
+
+
+
+(function(){
+ 
+var App = angular.module("module", []);
+    App.controller("controller", function ($scope, $http, $interval) {
+
+        
+        $http({
+            method: 'GET',
+            url: 'http://forwardingenuity.com/json_competitions.php'
+        })
+        .then(function (response) {
+            $scope.prize = (response.data)[0].winning;
+            window.localStorage.setItem("time",(response.data)[0].ending)
+            // alert("Here");
+             console.log(response.data);
+            // window.localStorage.setItem('response1',response.data)
+  
+$("#counting")
+  .countdown(window.localStorage.getItem("time"), function(event) {
+	  $(".days").text(event.strftime('%D'));
+	  $(".hours").text(event.strftime('%H'));
+
+	  $(".minutes").text(event.strftime('%M'));
+	  $(".seconds").text(event.strftime('%S'))
+   /* $(this).text(
+      event.strftime('%D days %H:%M:%S')
+    );*/
+  });
+        })
+
+     
+     
+
+    })
+
+}());
