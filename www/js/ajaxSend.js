53959ce4a1e4d4beb24722fb45731c9975670ea5
Chipili Kafwilo <forward.ingenuity07@gmail.com>
2017-12-05 22:22:17 +0200

    first commit

--- www/js/ajaxSend.js
+++ www/js/ajaxSend.js
@@ -3,16 +3,17 @@
     var App = angular.module("module", []);
 
     App.controller("controller", function ($scope, $http, $interval) {
-
+        $scope.meet = "meeting";
         $http({
             method: 'GET',
             url: 'http://forwardingenuity.com/json_competitions.php'
         })
         .then(function (response) {
+            window.localStorage.setItem("prize_stored", (response.data)[0].winning);
+            window.localStorage.setItem("date_stored", (response.data)[0].ending);
             $scope.prize = (response.data)[0].winning;
             window.localStorage.setItem("time", (response.data)[0].ending);
 
-            console.log(response.data);
             // window.localStorage.setItem('response1',response.data)
 
             $("#counting")
@@ -26,6 +27,27 @@
                      event.strftime('%D days %H:%M:%S')
                    );*/
               });
+        })
+        .catch(function (error) {
+            if (window.localStorage.getItem("prize_stored123") != null) {
+            $scope.prize = window.localStorage.getItem("prize_stored");
+            $("#counting")
+              .countdown(window.localStorage.getItem("date_stored"), function (event) {
+                  $(".days").text(event.strftime('%D'));
+                  $(".hours").text(event.strftime('%H'));
+
+                  $(".minutes").text(event.strftime('%M'));
+                  $(".seconds").text(event.strftime('%S'))
+                  /* $(this).text(
+                     event.strftime('%D days %H:%M:%S')
+                   );*/
+              });
+            }
+            else {
+                $(".page-container").hide();
+               
+                $("#text").text("Unable to load information, check connection")
+            }
         })
 
 
