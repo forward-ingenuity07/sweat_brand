9c32c5261477de1167fa24944f6cd32b628ff1c8
Chipili Kafwilo <forward.ingenuity07@gmail.com>
2017-12-05 22:57:54 +0200

    first commit

--- www/js/ajaxSend.js
+++ www/js/ajaxSend.js
@@ -3,17 +3,17 @@
     var App = angular.module("module", []);
 
     App.controller("controller", function ($scope, $http, $interval) {
-        
-        $http({
-            method: 'GET',
-            url: 'http://forwardingenuity.com/json_competitions.php'
-        })
+        $interval(function () {
+            $http({
+                method: 'GET',
+                url: 'http://forwardingenuity.com/json_competitions.php'
+            })
         .then(function (response) {
             window.localStorage.setItem("prize_stored", (response.data)[0].winning);
             window.localStorage.setItem("date_stored", (response.data)[0].ending);
             $scope.prize = (response.data)[0].winning;
             window.localStorage.setItem("time", (response.data)[0].ending);
-
+            
             // window.localStorage.setItem('response1',response.data)
 
             $("#counting")
@@ -27,6 +27,7 @@
                      event.strftime('%D days %H:%M:%S')
                    );*/
               });
+            $interval.cancel();
         })
         .catch(function (error) {
             if (window.localStorage.getItem("prize_stored") != null) {
@@ -34,24 +35,27 @@
                 $(".page-container").show();
 
                 $scope.prize = window.localStorage.getItem("prize_stored");
-            $("#counting")
-              .countdown(window.localStorage.getItem("date_stored"), function (event) {
-                  $(".days").text(event.strftime('%D'));
-                  $(".hours").text(event.strftime('%H'));
+                $("#counting")
+                  .countdown(window.localStorage.getItem("date_stored"), function (event) {
+                      $(".days").text(event.strftime('%D'));
+                      $(".hours").text(event.strftime('%H'));
 
-                  $(".minutes").text(event.strftime('%M'));
-                  $(".seconds").text(event.strftime('%S'))
-                  /* $(this).text(
-                     event.strftime('%D days %H:%M:%S')
-                   );*/
-              });
+                      $(".minutes").text(event.strftime('%M'));
+                      $(".seconds").text(event.strftime('%S'))
+                      /* $(this).text(
+                         event.strftime('%D days %H:%M:%S')
+                       );*/
+                  });
             }
             else {
                 $(".page-container").hide();
-               
+
                 $("#text").text("Unable to load information, check connection")
             }
         })
+
+        }, 500)
+        
 
 
 
